124c930d6ee9dbb2cd94a9dfd8c2c75d85c60dc3
Scott White <spwhite1337@gmail.com>
2020-07-25 12:11:08 -0400

    Project skeleton

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jd-battery-sizing',
+    version='1.0',
+    description='Battery Sizing for Dad',
+    author='Scott P. White',
+    author_email='spwhite1337@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'xrld',
+        'flask',
+        'flask-wtf',
+        'flask-sqlalchemy',
+        'flask-migrate',
+        'flask-login',
+        'plotly',
+        'dash',
+        'ipykernel',
+        'tqdm',
+    ]
+)
