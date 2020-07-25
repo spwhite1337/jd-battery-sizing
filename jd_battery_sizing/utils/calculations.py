35fe6e5450097ebc59208a3b430ff4a3432f7e2e
Scott White <spwhite1337@gmail.com>
2020-07-25 16:09:10 -0400

    Max daily energy above

--- jd_battery_sizing/utils/calculations.py
+++ jd_battery_sizing/utils/calculations.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+
+from jd_battery_sizing.utils.wrangle import daily_aggregation
 from config import logger
 
 
@@ -22,30 +24,35 @@ def energy_by_demand(df: pd.DataFrame, interval_min: int = 15):
         record = {}
 
         # Energy above
-        df_above = df[df[calc_col] < cutoff]
+        df_above = df[df[calc_col] >= cutoff]
         if df_above.shape[0] == 0:
-            record.update({'daily_energy_above_kwh': 0., 'daily_min_above': 0})
+            total_time_min, total_energy_kwh, max_daily_energy_above = 0., 0, 0.
         else:
             total_time_min = df_above.shape[0] * interval_min
             total_energy_kwh = df_above[calc_col].sum() * total_time_min / 60
-            record.update({
-                'daily_energy_above_kwh': total_energy_kwh / total_time_days,
-                'daily_min_above': total_time_min / total_time_days
-            })
+
+            df_daily = daily_aggregation(df_above, only_weekdays=False)
+            max_daily_energy_above = df_daily[calc_col].max() * 24
+
+        record.update({
+            'daily_energy_above_kwh': total_energy_kwh / total_time_days,
+            'daily_min_above': total_time_min / total_time_days,
+            'max_daily_energy_above_kWh': max_daily_energy_above
+        })
 
         # Energy below
-        df_below = df[df[calc_col] >= cutoff]
+        df_below = df[df[calc_col] < cutoff]
         if df_below.shape[0] == 0:
-            record.update({'daily_energy_below_kwh': 0., 'daily_min_below': 0})
+            total_time_min, total_energy_kwh = 0., 0
         else:
             total_time_min = df_below.shape[0] * interval_min
             total_energy_kwh = df_below[calc_col].sum() * total_time_min / 60
-            record.update({
-                'daily_energy_below_kwh': total_energy_kwh / total_time_days,
-                'daily_min_below': total_time_min / total_time_days
-            })
+        record.update({
+            'daily_energy_below_kwh': total_energy_kwh / total_time_days,
+            'daily_min_below': total_time_min / total_time_days,
+        })
 
-        record['cutoff'] = cutoff
+        record['ApparentPowerCutoff_KVA'] = cutoff
 
         # Gather
         records.append(record)
@@ -53,7 +60,7 @@ def energy_by_demand(df: pd.DataFrame, interval_min: int = 15):
 
     # Other calcs
     df_output['total'] = df_output['daily_energy_above_kwh'] + df_output['daily_energy_below_kwh']
-    df_output['FractionEnergyAbove_kWh_'] = df_output['daily_energy_above_kwh'] / df_output['total']
+    df_output['FractionEnergyAbove'] = df_output['daily_energy_above_kwh'] / df_output['total']
     df_output['total'] = df_output['daily_min_above'] + df_output['daily_min_below']
     df_output['FractionTimeAbove_min_'] = df_output['daily_min_above'] / df_output['total']
     df_output = df_output.drop('total', axis=1)
